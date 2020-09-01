10cbffc5516cd7a17305677915ce1f5851f120a7
Chris Kanich <ckanich@uic.edu>
2020-08-31 21:45:33 -0500

    prettier and smaller static-example

--- lecture2/static-example.c
+++ lecture2/static-example.c
@@ -4,26 +4,19 @@ int not_defined_here;
 char message[] = "hello world";
 static int invocations = 0;
 
-void hello_world(int increment)
-{
-    int i;
-    static int first_time = 0;
-    for (i = 0; i < increment; i++)
-    {
-        puts(message);
-        invocations++;
-    }
+void hello_world(int increment) {
+  static int first_time = 0;
+  if (increment >= 0) {
+    puts(message);
+    invocations++;
     fprintf(stderr, "I have printed to the screen %d times.\n", invocations);
-    if (first_time == 0)
-    {
-        first_time++;
-        hello_world(1);
-    }
-    else
-        first_time++;
+
+    hello_world(increment-1);
+  }
+  if (first_time == 0) {
+    fprintf(stderr, "this is the end of the first invocation of hello_world\n");
+    first_time++;
+  }
 }
 
-int main()
-{
-    hello_world(2);
-}
+int main() { hello_world(3); }
